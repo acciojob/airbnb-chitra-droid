22f4f96c68dc44b58c942138a4628c65b22cb3b8
chitra-droid <ucb9460@gmail.com>
2024-09-13 12:09:41 +0530

    c

--- src/main/java/com/driver/repository/HotelManagementRepository.java
+++ src/main/java/com/driver/repository/HotelManagementRepository.java
@@ -25,10 +25,10 @@ public class HotelManagementRepository {
    }
 
    public Integer addUser(User user) {
-      if(user!=null) {
+
          UserDB.put(user.getaadharCardNo(), user);
          return user.getaadharCardNo();
-      }
-      return -1;
+
+
    }
 }
